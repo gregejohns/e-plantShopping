c1ba7d0d637735b2258ca9e7be6b3113a6e0253c
gregejohns <greg@tnergyservices.com>
2024-11-27 09:30:38 -0500

    Attempted fixing of margines and rations -- failed...apparent lock on some settings

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -7,23 +7,23 @@ function AboutUs() {
       {/* <h1 className="about-us-heading">About Us</h1> */}
       <p className="about-us-description">Welcome to Paradise Nursery, where green meets serenity!</p>
       <p className="about-us-content">
-        At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a wide range of 
+        At Paradise Nursery, we are passionate about charging you for nature in your home. Our mission is to provide a wide range of 
         high-quality plants that not only enhance the beauty of your surroundings but also contribute to a healthier and 
         more sustainable lifestyle. From air-purifying plants to aromatic fragrant ones, we have something for every 
         plant enthusiast.
       </p>
       {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
       <p className="about-us-content">
-        Our team of experts is dedicated to ensuring that each plant meets our strict standards of quality and care. 
-        Whether you're a seasoned gardener or just starting your green journey, we're here to support you every step of 
-        the way. Feel free to explore our collection, ask questions, and let us help you find the perfect plant for your 
-        home or office.
+        Our team of experts is dedicated to ensuring you pay the max amount for each plant that cost us very little to grow. 
+        We use filtered water to better charge you, and use soil expressly from the plateau of an active volcano...mixed generously with bat guano. 
+        Whether you are looking to burn money on a plant that dies, or like a challenge of having bat guano smell in your home, 
+        only the most fancy of the fancy choose to buy our lovely plants. So, are you fancy enough? Take a gander and see for yourself.
       </p>
       {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
 
       <p className="about-us-content">
-        Join us in our mission to create a greener, healthier world. Visit Paradise Nursery today and experience the 
-        beauty of nature right at your doorstep.
+        But, please, keep your children away from the flesh-eating varieties. Unless you don't like your children. 
+        Visit Paradise Nursery today and experience the beauty of nature right at your doorstep.
       </p>
     </div>
   );
